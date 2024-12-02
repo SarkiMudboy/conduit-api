e9cf623b43d6ed3ffac5b0306cc160982da9b0ab
abdul <abdoolihima@gmail.com>
2024-12-02 23:23:47 +0100

    Fix csrf token issues/changes to endpoint for getting presigned url

--- conduit/share/serializers.py
+++ conduit/share/serializers.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from abstract.apis.aws.handlers import S3AWSHandler
 from rest_framework import serializers
 from storage.models import Object
@@ -6,19 +8,39 @@ from storage.models import Object
 class FileDataSerializer(serializers.Serializer):
 
     filename = serializers.CharField(max_length=2000)
-    filesize = serializers.DecimalField(decimal_places=2)
+    filesize = serializers.IntegerField(required=True)
+
+    # validate size and depth of dir here...
 
 
 class UploadPresignedURLSerializer(serializers.Serializer):
 
-    file = FileDataSerializer(required=True)
-    resource_uid = serializers.PrimaryKeyRelatedField(
-        queryset=Object.objects.filter(),  # ensure this is owned by owner
+    file = FileDataSerializer(many=True, required=True)
+    resource = serializers.PrimaryKeyRelatedField(
+        queryset=Object.objects.all(),
         required=False,
     )
 
+    def get_fields(self):
+
+        fields = super().get_fields()
+        if fields.get("resource"):
+            filters = {
+                "owner": self.context.get("owner"),
+                "drive__uid": self.context.get("drive"),
+            }
+            fields["resource"].queryset = Object.objects.filter(**filters)
+        return fields
+
     def get_url(self) -> str:
 
-        filename = self.validated_data.file.name
+        key = self.validated_data.get("file")[0]["filename"]
+
+        # append the dir path here
+        object: Optional[Object] = self.validated_data.get("resource")
+        if object:
+            key = object.get_file_path(key)
+
+        key = f'{self.context.get("drive")}/{key}'
         handler = S3AWSHandler()
-        return handler.get_upload_presigned_url(filename)
+        return handler.get_upload_presigned_url(key)
--- conduit/share/views.py
+++ conduit/share/views.py
@@ -2,8 +2,10 @@ from django.http import HttpRequest
 from django.shortcuts import HttpResponse
 from rest_framework.decorators import action
 from rest_framework.exceptions import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from storage.models import Drive
 
 from .serializers import UploadPresignedURLSerializer
 
@@ -11,15 +13,24 @@ from .serializers import UploadPresignedURLSerializer
 class ShareViewSet(ViewSet):
 
     serializer_class = UploadPresignedURLSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = (
+        Drive.objects.prefetch_related("members")
+        .select_related("owner")
+        .filter(is_active=True)
+    )
+
+    # def get_object
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
-        context["drive_uid"] = self.kwargs.get("drives_uid")
+        context["drive"] = self.kwargs.get("drives_uid")
+        context["owner"] = self.request.user
         return context
 
-    @action(methods=("GET"), detail=False, url_path="get-upload-url")
+    @action(methods=["POST"], detail=False, url_path="get-upload-url")
     def get_upload_url(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
-        serializer = self.serializer_class(data=request.GET)
+        serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         url = serializer.get_url()
         return Response(data={"url": url}, status=status.HTTP_200_OK)
